4fc64e6f661abf3c438399361267025601bf9b29
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2017-01-15 12:54:12 +0000

    commiting changes

--- StackUsingLinkedList.java
+++ StackUsingLinkedList.java
@@ -51,6 +51,7 @@ public class StackUsingLinkedList {
         System.out.println("popped: " + s.pop());
         s.display();
         System.out.println("top: " + s.peek());
+        // testing
     }
 }
 // tested manually
